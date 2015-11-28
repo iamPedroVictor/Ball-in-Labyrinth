ca1bd353e0bb8b6c2cbc23b9fa06ff570defde65
Pedro Victor <pedro.victor.ferreira@outlook.com.br>
2015-11-28 00:25:01 -0400

    Unity - Projeto
    Arquivos do projeto do Unity 5.2.1f1

---
+++ Ball in labyrinth/Assets/ColliderFunctions.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColliderFunctions : MonoBehaviour {
+
+	public string _ObjectTag;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	IEnumerator Espere(){
+		yield return new WaitForSeconds (2.5f);
+		Application.LoadLevel(Application.loadedLevel);
+	}
+	
+	void OnCollisionEnter(Collision collision){
+		if ( (collision.gameObject.tag == "Ball") && (_ObjectTag == "Exit") ) {
+			Debug.Log("Parabens voce passou na fase");
+		}
+		if ( (collision.gameObject.tag == "Ball") && (_ObjectTag == "Fall") ) {
+			Debug.Log("Voce Caiu tente novamente");
+			StartCoroutine(Espere());
+		}
+		
+	}
+
+
+}
---
+++ Ball in labyrinth/Assets/MouseControl.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseControl : MonoBehaviour {
+	private float _sensitivity;
+	private Vector3 _mouseReference;
+	private Vector3 _mouseOffset;
+	private Vector3 _rotation;
+	private bool _isRotating;
+
+	private Transform CaixaRef;
+
+	void Start ()
+	{
+		CaixaRef = GameObject.Find ("Caixa").GetComponent<Transform> ();
+		_sensitivity = 0.05f;
+		_rotation = Vector3.zero;
+	}
+	
+	void Update()
+	{
+		if(_isRotating){
+
+			// offset
+			_mouseOffset = (Input.mousePosition - _mouseReference);
+			// apply rotation
+			_rotation.z = (-(_mouseOffset.x) * _sensitivity);
+			_rotation.x = ((_mouseOffset.y) * _sensitivity);
+			// rotate
+			//transform.Rotate(_rotation);
+			transform.eulerAngles += _rotation;
+			//transform.Rotate(_rotation);
+			// store mouse
+			_mouseReference = Input.mousePosition;
+		}
+
+		if (Input.GetMouseButtonDown (0)) {
+			// rotating flag
+			_isRotating = true;
+			
+			Debug.Log ("Pegando a posicao do mouse");
+			
+			// store mouse
+			_mouseReference = Input.mousePosition;
+			Debug.Log (_mouseReference);
+		} else if (Input.GetMouseButtonUp (0)) {
+			// rotating flag
+			_isRotating = false;
+		}
+	}
+	
+}
